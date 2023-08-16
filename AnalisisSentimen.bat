7fc2d491b9e9c26caa506954302ed980de6e385e
Fahri Putra <fahriputra000@gmail.com>
2023-08-17 00:51:02 +0700

    Update AnalisisSentimen.bat

--- AnalisisSentimen.bat
+++ AnalisisSentimen.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d D:\CODING\SMT6\AnalisisSentimen_Streamlit\AnalisisSentimenKanjuruhanMalang
-streamlit run main.py
+streamlit run D:\CODING\SMT6\AnalisisSentimen_Streamlit\AnalisisSentimenKanjuruhanMalang\main.py 
